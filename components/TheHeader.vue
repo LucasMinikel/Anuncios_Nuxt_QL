9128c850d8aa11fef88504e69e761cfd7a027238
Lucas Minikel <lucasminikel.m@gmail.com>
2022-01-24 19:25:22 -0300

    Index e Header

---
+++ components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="py-6 flex items-center justify-between">
+        <nuxt-link :to="{ name: 'index' }" exact-active-class="text-blue-500"
+            >Anúncios</nuxt-link
+        >
+        <div class="-mx-4 flex items-center flex-wrap">
+            <span class="mx-4 font-bold">Alex</span>
+            <nuxt-link
+                :to="{ name: 'login' }"
+                class="mx-4"
+                exact-active-class="text-blue-500"
+                >Entrar</nuxt-link
+            >
+            <a href="" class="mx-4" exact-active-class="text-blue-500">Sair</a>
+            <a href="" class="mx-4" exact-active-class="text-blue-500"
+                >Gerenciador</a
+            >
+            <a href="" class="mx-4" exact-active-class="text-blue-500"
+                >Criar anúncio</a
+            >
+        </div>
+    </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <Tutorial/>
+    <div>
+        <div class="text-center py-10">
+            <h1 class="text-4xl font-bold mb-2">
+                Encontre os melhores anúncios
+            </h1>
+            <p class="text-gray-600 font-medium">Para você</p>
+        </div>
+        <div>Lista de anuncios</div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+export default {};
 </script>
